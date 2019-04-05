ff82ade1f7fff255eabb4ff26cb015a92cebe1e3
kzkz <kzkz.ozn@gmail.com>
2019-04-05 15:26:43 +0900

    todo component focuses itself

---
+++ src/components/Todo.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="todo">
+    <div v-if="!todo.isEditing" v-text="todo.content" @click="todo.isEditing = true; forceFocus()"></div>
+    <input ref="todo" v-show="todo.isEditing" type="text" v-model="todo.content" @blur="todo.isEditing = false">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Todo',
+  model: {
+    prop: 'todo', //親からもらったデータの呼び名をvalueからtodoに変更
+    event: 'input'
+  },
+  props: {
+    todo: Object //親からもらったtodoオブジェクト    
+  },
+	methods: {
+		forceFocus: function() {
+      this.$nextTick(() => this.$refs.todo.focus())
+		}
+	}
+}
+</script>
---
+++ src/views/Playground.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="playground">
+    <h1>This is an playground page</h1>
+    <ul>
+      <li v-for="(todo, index) in todos" :key="index">
+        <Todo v-model="todos[index]"/>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Todo from "@/components/Todo.vue";
+
+export default {
+  name: "home",
+  components: {
+    Todo,
+  },
+  data: function() {
+    return {
+      todos: [
+        {
+          id:"1",
+          content: "todo1",
+          isDone: false,
+          isEditing: false,
+          htmlId: "todo1"
+        },
+        {
+          id:"2",
+          content: "todo2",
+          isDone: false,
+          isEditing: false,
+          htmlId: "todo2"
+        },
+        {
+          id:"3",
+          content: "todo3",
+          isDone: false,
+          isEditing: false,
+          htmlId: "todo3"
+        }
+      ]
+    };
+  }
+};
+</script>
